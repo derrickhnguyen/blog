e1b759b935465931d0d85008edda07c252d0b1ed
Derrick Nguyen <derrickhnguyen@outlook.com>
2020-07-16 15:02:05 -0700

    feat(gql): Created updateCurrentUserProfileImageUrlMutation (#8)

--- src/contextTypes.ts
+++ src/contextTypes.ts
@@ -1,8 +1,10 @@
 import { User, PrismaClient } from '@prisma/client'
+import { v2 as Cloudinary } from 'cloudinary'
 import { Request } from 'express'
 import { ContextParameters } from 'graphql-yoga/dist/types'
 
 export interface ContextType extends ContextParameters {
+  cloudinary: typeof Cloudinary
   prisma: PrismaClient
   request: Request & { currentUser?: User; currentUserId?: string }
 }
